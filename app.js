60bf25b56284c84439ef6b68b5c0dd9146d0ec21
Fengqing Mai <maifengqing@gmail.com>
2017-06-21 01:35:12 -0700

    initial version

---
+++ app.js
@@ -0,0 +1,121 @@
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var express = require("express");
+var mongoose = require("mongoose");
+var expressSanitizer = require("express-sanitizer");
+var app = express();
+
+
+
+mongoose.connect("mongodb://localhost:restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+// Blog.create({
+//     title: "Test dog",
+//     image: "https://www.pexels.com/photo/animal-dog-pet-focus-7720/",
+//     body: "This is a blog post"
+// });
+
+
+// RESTFUL ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+// RESTFUL ROUTES
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("index", {blogs: blogs});
+       }
+    });
+});
+
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+  res.render("new"); 
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    // then, redirect
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            console.log(err);
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+      if (err) {
+            console.log(err);
+            res.redirect("/blogs");
+      } else {
+            res.render("show", {blog: foundBlog});
+           
+      }
+    });
+})
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    // res.send("update");
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog,function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("SERVER IS RUNNING"); 
+});
---
+++ readme.md
@@ -0,0 +1,43 @@
+#RESTful Routing
+
+##Intro
+* Define REST and explain WHY it matters
+* List all 7 RESTful routes
+* Show example of RESTful routing in practice
+
+#Blog Index
+* Setup the Blog App
+* Create the Blog model
+* Add INDEX route and template
+* Add Simple Nav Bar
+
+REST - a mapping between HTTP routes and CRUD
+
+CREATE  
+READ    /allBlogs
+UPDATE  /updateBlog/:id
+DESTROY /destroyBlog/:id
+
+#Basic LAyout
+* Add header and Hooter Partials
+* Include Semantic UI
+* Add Simple Nav
+
+#SHOWtime
+* Add Show route
+* Add Show template
+* Add links to show page
+* Style show template
+
+
+#Edit/Update
+* Add Edit Route
+* Add Edit Form
+* Add Update Route
+* Add Update Form
+* Add Methoud-Override
+
+#Final Updates
+* Sanitize blog body
+* Style Index
+* Update REST table
